268dd5fa090b0c5a7e6256db72b37a8f52e92acf
heewls <136988129+heewls@users.noreply.github.com>
2025-06-28 22:37:16 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 36.55 ms, Memory: 82.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.js
@@ -0,0 +1,11 @@
+// filter 이용
+// 다음 요소랑 같지 않으면 얕은 복사본 생성
+
+function solution(arr)
+{
+    const answer = [];
+    for (let i = 0; i < arr.length; i++) {
+        if (arr[i] !== arr[i + 1]) answer.push(arr[i]);
+    }
+    return answer;
+}
