56ed6e60c949f92bf19cd14c1698c9d1cf73f0a8
heewls <136988129+heewls@users.noreply.github.com>
2025-07-01 04:08:05 +0900

    [level 1] Title: 서울에서 김서방 찾기, Time: 0.03 ms, Memory: 32.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12919. 서울에서 김서방 찾기/서울에서 김서방 찾기.js
@@ -0,0 +1,10 @@
+// function solution(seoul) {
+//     for (let i = 0; i < seoul.length; i++) {
+//         if(seoul[i] === 'Kim') return `김서방은 ${i}에 있다`
+//     }
+// }
+
+function solution(seoul) {
+    const idx = seoul.indexOf('Kim');
+    return `김서방은 ${idx}에 있다`
+}
