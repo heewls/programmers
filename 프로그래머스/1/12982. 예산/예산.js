fdd392ff5d294c8247dc8ccb9f1458fa1b8ea2bc
heewls <136988129+heewls@users.noreply.github.com>
2025-07-08 04:02:26 +0900

    [level 1] Title: 예산, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/1/12982. 예산/예산.js
@@ -0,0 +1,15 @@
+function solution(d, budget) {
+    let count = 0;
+    d.sort((a, b) => a - b);
+    
+    for (let i = 0; i < d.length; i++) {
+        if(budget >= d[i]) {
+            budget -= d[i];
+            count ++;
+        } else {
+            break;
+        }
+    }
+    
+    return count;
+}
