bd79778334e75ec491c0d04356e35bd219a8333e
heewls <136988129+heewls@users.noreply.github.com>
2025-07-12 16:49:18 +0900

    [level 1] Title: 최소직사각형, Time: 8.95 ms, Memory: 41 MB -BaekjoonHub

---
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.js
@@ -0,0 +1,21 @@
+// 이중 배열 중 더 긴 게 가로
+
+// function solution(sizes) {
+//     let maxW = 0, maxH = 0;
+    
+//     for (const [w, h] of sizes) {
+//         const [width, height] = w > h ? [w, h] : [h, w];
+//         if (width > maxW) maxW = width;
+//         if (height > maxH) maxH = height;
+//     }
+    
+//     return maxW * maxH;
+// }
+
+function solution(sizes) {
+    sizes = sizes.map(([w, h]) => w > h ? [w, h] : [h, w]);
+    const maxW = Math.max(...sizes.map(([w]) => w));
+    const maxH = Math.max(...sizes.map(([, h]) => h));
+    
+    return maxW * maxH;
+}
