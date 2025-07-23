f658fc49ea9ab54f228a40963bff763389bc1a4a
heewls <136988129+heewls@users.noreply.github.com>
2025-07-23 19:35:28 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 34.39 ms, Memory: 35.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.js
@@ -0,0 +1,6 @@
+// localeCompare, 문자열은 a - b 안됨
+// n번째 문자열이 같으면 전체 문자열 사전순으로
+
+function solution(strings, n) {
+    return strings.sort((a, b) => a[n] === b[n] ? a.localeCompare(b) : a[n].localeCompare(b[n]));
+}
