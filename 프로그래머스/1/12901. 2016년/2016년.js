9a0a80f293f247b2c5932d2336c3cf2e7f7209ee
heewls <136988129+heewls@users.noreply.github.com>
2025-07-29 04:33:31 +0900

    [level 1] Title: 2016년, Time: 116.89 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/1/12901. 2016년/2016년.js
@@ -0,0 +1,9 @@
+// getDay
+// error : new Date(2016, a, b)로 했는데 FRI이 나옴 => monthIndex는 0부터
+
+function solution(a, b) {
+    const week = ["SUN","MON", "TUE", "WED", "THU", "FRI", "SAT"];
+    const date = new Date(2016, a - 1, b);
+    
+    return week[date.getDay()];
+}
