db734997afa4eb655914a7a682ba8d312412d61f
heewls <136988129+heewls@users.noreply.github.com>
2025-08-01 01:51:59 +0900

    [level 1] Title: 내적, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/1/70128. 내적/내적.js
+++ 프로그래머스/1/70128. 내적/내적.js
@@ -1,13 +1,3 @@
-// function solution(a, b) {
-//     let answer = 0;
-    
-//     for (let i = 0; i < a.length; i++) {
-//         answer += a[i] * b[i];
-//     }
-    
-//     return answer;
-// }
-
 function solution(a, b) {
-    return a.reduce((a, c, idx) => a + c * b[idx], 0);
+    return a.reduce((a, c, i) => a + c * b[i], 0);
 }
