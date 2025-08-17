1cbdcf50c4fa30390d18fd38d6e242fe8ff34611
heewls <136988129+heewls@users.noreply.github.com>
2025-08-17 20:02:25 +0900

    [level 2] Title: 숫자의 표현, Time: 2.08 ms, Memory: 36.3 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.js
@@ -1,13 +1,11 @@
 // 시작 숫자를 기준으로 sum
 // for 이중 반복문(첫 번째 반복문 : 덧셈 시작하는 숫자, 두 번째 반복문 : 시작하는 숫자부터 n까지 sum)
-// error : 시간초과, sum이 n과 같을 때만 break 하다 보니 sum이 n보다 커져도 n까지 계속 반복
-// error : 시간초과, n/2보다 크면 어차피 n 넘어감
-// 
+// error : 시간초과, sum이 n과 같을 때만 break 하다 보니 sum이 n보다 커져도 n까지 계속 반복 => for 조건문에 sum < n
 
 function solution(n) {
-    let count = 0;
+    let count = 1;
     
-    for (let i = 1; i <= n; i++) {
+    for (let i = 1; i < n; i++) {
         let sum = i;
         
         for (let j = i + 1; sum < n; j++) {
